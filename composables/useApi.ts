aeea840e2d99333e29adf23e8b0ff2164da6140b
Juan Fidelis <juan.fidelis.ext@tlscontact.com>
2024-09-01 14:12:32 +0100

    Created useApi composable and the first function that calls the api using useFetch

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -4,16 +4,18 @@ export default function useApi()  {
   const config = useRuntimeConfig();
 
   return {
-    getSearch: async (query: SearchQuery): Promise<any> => {
-      const { data: response } = await useFetch(`${config.public.baseUrl}/current.json`,
+    getSearchList: async (query: SearchQuery): Promise<any> => {
+      const { data: response } = await useFetch(`${config.public.baseUrl}/search.json`,
         {
+          key: `locations-${query.q}`,
           query: {
             key: config.public.apiKey,
             ...query,
           },
+          server: false,
         },
       );
-      return response;
+      return response.value;
     }
   }
 }
